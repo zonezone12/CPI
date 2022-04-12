ad04e4ae10f6d066a3df94bb4f75d0eb0f0b7285
zonezone12 <72430447+zonezone12@users.noreply.github.com>
2022-04-12 17:51:58 +0800

    Add files via upload

--- CPI/GPCR/main_go.sh
+++ CPI/GPCR/main_go.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #Batch Job Paremeters
-#SBATCH --mail-user=rocklee2578@gmail.com
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH --job-name=GPCR_molft
+#SBATCH --job-name=GPCR
 #SBATCH --account=MST109174
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=2
-#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=16
 #SBATCH --partition=gp4d
 
 srun python main.py 
--- CPI/GPCR/molft.sh
+++ CPI/GPCR/molft.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #Batch Job Paremeters
-#SBATCH --mail-user=rocklee2578@gmail.com
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
 #SBATCH --job-name=GPCR_molft
 #SBATCH --account=MST109174
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --partition=gp4d
+#SBATCH --partition=gtest
 
 srun python mol_featurizer.py 
